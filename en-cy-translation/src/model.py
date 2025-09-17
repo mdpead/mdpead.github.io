cc3a057f88029abf2a41a027ca60a0f7a099ea5c
Matthew Pead <mdpead@gmail.com>
2025-09-17 14:02:40 +0100

    model improvements

--- en-cy-translation/src/model.py
+++ en-cy-translation/src/model.py
@@ -1,24 +1,22 @@
 from torch import nn
 import torch
 import math
-import copy
 
 
 class Linear(nn.Module):
     def __init__(self, in_features, out_features):
-        super(Linear, self).__init__()
+        super().__init__()
         self.weight = nn.Parameter(torch.randn(out_features, in_features))
         self.bias = nn.Parameter(torch.zeros(out_features))
 
     def forward(self, x):
         x = torch.matmul(x, self.weight.t()) + self.bias
-
         return x
 
 
 class PositionWiseFeedForward(nn.Module):
     def __init__(self, d_model, d_ff):
-        super(PositionWiseFeedForward, self).__init__()
+        super().__init__()
         self.linear1 = Linear(d_model, d_ff)
         self.linear2 = Linear(d_ff, d_model)
         self.relu = nn.ReLU()
@@ -32,7 +30,7 @@ class PositionWiseFeedForward(nn.Module):
 
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-6):
-        super(LayerNorm, self).__init__()
+        super().__init__()
         self.eps = eps
         self.alpha = nn.Parameter(torch.ones(d_model))
         self.bias = nn.Parameter(torch.zeros(d_model))
@@ -45,7 +43,7 @@ class LayerNorm(nn.Module):
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads):
-        super(MultiHeadAttention, self).__init__()
+        super().__init__()
         assert d_model % num_heads == 0
         self.num_heads = num_heads
         self.d_h = d_model // num_heads
@@ -66,16 +64,10 @@ class MultiHeadAttention(nn.Module):
         K_mh = K.reshape(K.shape[0], K.shape[1], self.num_heads, self.d_h).permute(0, 2, 1, 3)
         V_mh = V.reshape(V.shape[0], V.shape[1], self.num_heads, self.d_h).permute(0, 2, 1, 3)
 
-        # Create attention mask
-        causal_mask = (
-            torch.tril(torch.ones(q.shape[1], k.shape[1]).bool()).unsqueeze(0).unsqueeze(1)
-        )
-        # padding_mask = (q_padding_mask.unsqueeze(-1) & k_padding_mask.unsqueeze(-2)).unsqueeze(1)
-        # attn_mask = (causal_mask & padding_mask).expand(-1, self.num_heads, -1, -1)
-
         # Calculate attention matrices
         attn_raw = torch.matmul(Q_mh, K_mh.transpose(-1, -2)) / math.sqrt(self.d_h)
-        attn_masked = attn_raw.masked_fill(~attn_mask, float("-inf"))
+        attn_mask_mh = attn_mask.expand(-1, self.num_heads, -1, -1)
+        attn_masked = attn_raw.masked_fill(~attn_mask_mh, float("-inf"))
         attn = torch.softmax(attn_masked, -1)
 
         # Apply to value vectors and recombine
@@ -90,7 +82,7 @@ class MultiHeadAttention(nn.Module):
 
 class Embedding(nn.Module):
     def __init__(self, vocab_size, d_model):
-        super(Embedding, self).__init__()
+        super().__init__()
         self.E = nn.Parameter(torch.rand(vocab_size, d_model))
 
     def forward(self, x):
@@ -100,7 +92,7 @@ class Embedding(nn.Module):
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_length):
-        super(PositionalEncoding, self).__init__()
+        super().__init__()
         self.register_buffer("PE", self.create_pe(d_model, max_length))
 
     def create_pe(self, d_model, max_length):
@@ -123,27 +115,123 @@ class PositionalEncoding(nn.Module):
 
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_ff):
+        super().__init__()
         self.attention = MultiHeadAttention(d_model, num_heads)
-        self.layer_norm_one = LayerNorm(d_model)
         self.feedforward = PositionWiseFeedForward(d_model, d_ff)
-        self.layer_norm_two = LayerNorm(d_model=d_model)
+        self.layer_norms = nn.ModuleList([LayerNorm(d_model) for _ in range(0, 2)])
 
-    def forward(self, src, src_padding_mask):
-        src = self.attention(src, src, src, src_padding_mask, src_padding_mask)
-        src = self.layer_norm_one(src)
-        src = self.feedforward(src)
-        src = self.layer_norm_two(src)
-        return src
+    def forward(self, src, attn_mask):
+        x = src
+        x = self.attention(x, x, x, attn_mask) + x
+        x = self.layer_norms[0](x)
+        x = self.feedforward(x)
+        x = self.layer_norms[1](x)
+        return x
 
 
 class Encoder(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, num_enc_layers):
+        super().__init__()
         self.layers = nn.ModuleList(
             [EncoderLayer(d_model, num_heads, d_ff) for _ in range(num_enc_layers)]
         )
 
-    def forward(self, src, src_padding_mask):
-        for layer in self.layers:
-            src = layer(src, src_padding_mask)
+    def make_attn_mask(self, q_padding_mask, k_padding_mask):
+        padding_mask = (q_padding_mask.unsqueeze(-1) & k_padding_mask.unsqueeze(-2)).unsqueeze(1)
+        return padding_mask
 
+    def forward(self, src, src_padding_mask):
+        attn_mask = self.make_attn_mask(src_padding_mask, src_padding_mask)
+        for layer in self.layers:
+            src = layer(src, attn_mask)
         return src
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff):
+        super().__init__()
+        self.self_attention = MultiHeadAttention(d_model, num_heads)
+        self.cross_attention = MultiHeadAttention(d_model, num_heads)
+        self.feedforward = PositionWiseFeedForward(d_model, d_ff)
+        self.layer_norms = nn.ModuleList([LayerNorm(d_model) for _ in range(0, 3)])
+
+    def forward(self, src, tgt, self_attn_mask, cross_attn_mask):
+        x = tgt
+        x = self.self_attention(tgt, tgt, tgt, self_attn_mask) + x
+        x = self.layer_norms[0](x)
+        x = self.cross_attention(x, src, src, cross_attn_mask) + x
+        x = self.layer_norms[1](x)
+        x = self.feedforward(x)
+        x = self.layer_norms[2](x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, num_dec_layers):
+        super().__init__()
+        self.layers = nn.ModuleList(
+            [DecoderLayer(d_model, num_heads, d_ff) for _ in range(num_dec_layers)]
+        )
+
+    def make_self_attn_mask(self, q_padding_mask, k_padding_mask):
+        padding_mask = (q_padding_mask.unsqueeze(-1) & k_padding_mask.unsqueeze(-2)).unsqueeze(1)
+        causal_mask = (
+            torch.tril(torch.ones(q_padding_mask.shape[1], k_padding_mask.shape[1]).bool())
+            .unsqueeze(0)
+            .unsqueeze(1)
+        )
+        self_attn_mask = causal_mask & padding_mask
+        return self_attn_mask
+
+    def make_cross_attn_mask(self, q_padding_mask, k_padding_mask):
+        cross_attn_mask = (q_padding_mask.unsqueeze(-1) & k_padding_mask.unsqueeze(-2)).unsqueeze(1)
+        return cross_attn_mask
+
+    def forward(self, src, tgt, src_padding_mask, tgt_padding_mask):
+        # Make attention masks
+        self_attn_mask = self.make_self_attn_mask(tgt_padding_mask, tgt_padding_mask)
+        cross_attn_mask = self.make_cross_attn_mask(tgt_padding_mask, src_padding_mask)
+
+        for layer in self.layers:
+            tgt = layer(src, tgt, self_attn_mask, cross_attn_mask)
+        return tgt
+
+
+class Output(nn.Module):
+    def __init__(self, d_model, vocab_size):
+        super().__init__()
+        self.linear = Linear(d_model, vocab_size)
+
+    def forward(self, tgt_dec):
+        x = self.linear(tgt_dec)
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self, d_model, num_heads, d_ff, num_enc_layers, num_dec_layers, vocab_size, max_length
+    ):
+        super().__init__()
+        self.embedding = Embedding(vocab_size, d_model)
+        self.positional_encoder = PositionalEncoding(d_model, max_length)
+        self.encoder = Encoder(d_model, num_heads, d_ff, num_enc_layers)
+        self.decoder = Decoder(d_model, num_heads, d_ff, num_dec_layers)
+        self.output = Output(d_model, vocab_size)
+
+    def encode(self, src, src_padding_mask):
+        src_enc = self.encoder(src, src_padding_mask)
+        return src_enc
+
+    def decode(self, src_enc, tgt, src_padding_mask, tgt_padding_mask):
+        tgt_dec = self.decoder(src_enc, tgt, src_padding_mask, tgt_padding_mask)
+        return tgt_dec
+
+    def forward(self, src, tgt, src_padding_mask, tgt_padding_mask):
+        src_emb = self.embedding(src)
+        src_pos_enc = self.positional_encoder(src_emb)
+        src_enc = self.encode(src_pos_enc, src_padding_mask)
+        tgt_emb = self.embedding(tgt)
+        tgt_pos_enc = self.positional_encoder(tgt_emb)
+        tgt_dec = self.decode(src_enc, tgt_pos_enc, src_padding_mask, tgt_padding_mask)
+        out = self.output(tgt_dec[:, -1, :])
+        return out
